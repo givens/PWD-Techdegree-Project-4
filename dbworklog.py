658d8144dec546a69b11c0f9b0f97783cc1fc210
BG <bgivens@pm.me>
2018-11-28 02:57:46 -0500

    Changes notes input.  Fixed date search error.

--- dbworklog.py
+++ dbworklog.py
@@ -7,13 +7,12 @@ View for Database Worklog
 from collections import OrderedDict
 import datetime
 import os
-import pdb
 import sys
+import time
 
-from utils import *
+import utils
 import models
-
-fmt = '%Y%m%d'
+from utils import fmt
 
 
 def clear():
@@ -52,26 +51,15 @@ def search_menu():
     search.menu_loop()
 
 
-def result_menu(tasks_id):
+def result_menu(ids):
     """Result menu."""
-    result = ResultMenu(tasks_id)
+    result = ResultMenu(ids)
     result.menu_loop()
 
 
-    utils enter_task({
-        "employee": employee,
-        "taskname": taskname,
-        "minutes": minutes,
-        "notes": notes,
-        "date": date,
-    })
-
 def enter_task():
     """Add an entry."""
-
-    # Enter a new task from keyboard.
-    # Create in db.
-
+    clear()
     # Employee
     while True:
         employee = input("Employee name:  ")
@@ -79,7 +67,6 @@ def enter_task():
             break
         else:
             print("Try again.")
-
     # Taskname
     while True:
         taskname = input("Task name:  ")
@@ -87,7 +74,6 @@ def enter_task():
             break
         else:
             print("Try again.")
-
     # Minutes
     while True:
         minutes = input("Minutes spent:  ")
@@ -97,24 +83,18 @@ def enter_task():
             print("Try again.")
         else:
             break
-
     # Notes
-    print("Enter any notes.  Press ctrl-d when finished.")
-    notes = sys.stdin.read().strip()
+    notes = input("Any notes:  ")
+    if not notes:
+        notes = "none"
 
     # Date
     date = datetime.datetime.now().date()
-
     return {"employee": employee,
             "taskname": taskname,
             "minutes": minutes,
             "notes": notes,
             "date": date}
-
-def quit():
-    """Quit work log."""
-    print("Quit.")
-    sys.exit(0)
 
 
 class Menu:
@@ -134,7 +114,6 @@ class Menu:
             choice = input("Which option do you choose?  ").lower().strip()
 
             if choice in self.menu:
-                #pdb.set_trace()
                 self.menu[choice]()
 
 
@@ -148,15 +127,24 @@ class MainMenu(Menu):
                 ('q', self.quit)]),
             "Main Menu")
 
-    def enter_task(self):
+    @staticmethod
+    def enter_task():
+        """Add an entry."""
         dict = enter_task()
-        create_task(dict)
+        utils.create_task(dict)
 
-    def search_menu(self):
-        search_menu()
+    @staticmethod
+    def search_menu():
+        """Search entries."""
+        search = SearchMenu()
+        search.menu_loop()
+#        search_menu()
 
-    def quit(self):
-        quit()
+    @staticmethod
+    def quit():
+        """Quit."""
+        print("Quit.")
+        sys.exit(0)
 
 class SearchMenu(Menu):
 
@@ -168,8 +156,14 @@ class SearchMenu(Menu):
                 ('r', self.find_date_range),
                 ('t', self.find_time_spent),
                 ('s', self.find_search_term),
-                ('m', main_menu)]),
+                ('m', self.main_menu)]),
             "Search Menu")
+
+    @staticmethod
+    def main_menu():
+        """Go to main menu."""
+        main = MainMenu()
+        main.menu_loop()
 
     def find_employee(self):
         """Find by employee."""
@@ -178,53 +172,55 @@ class SearchMenu(Menu):
         # User selects choice
         # Choice is searched for
         # Go to ResultMenu
-        employees = find_unique_employees()
-        employee = item_table(employees)
-        tasks_id = find_by_employee(employee)
-        result_menu(tasks_id)
+        employees = utils.find_unique_employees()
+        employee = item_table(employees, "Employees")
+        ids = utils.find_by_employee(employee)
+        if ids:
+            result_menu(ids)
+        else:
+            print("No entries.")
+            time.sleep(1)
 
 
     def find_date(self):
         """Find by date."""
-        # List unique dates in order
-        # Provide choice
-        # User selects choice
-        # Choice is searched for
-        # Go to ResultMenu
-        dates = find_unique_dates()
-        date = item_table(dates)
-        tasks_id = find_by_date(date)
-        result_menu(tasks_id)
+        dates = utils.find_unique_dates()
+        date = item_table(dates, "Dates")
+        date = datetime.datetime.strptime(date, fmt).date()
+        ids = utils.find_by_date(date)
+        if ids:
+            result_menu(ids)
+        else:
+            print("No entries.")
+            time.sleep(1)
 
 
     def find_date_range(self):
         """Find by date range."""
-        # Enter start date
-        # Enter end date
-        # Date range is searched for
-        # Go to ResultMenu
-
-        # Start Date
         while True:
             start_date = input("Enter start date (YYYYMMDD):  ")
             try:
-                start_date = datetime.datetime.strptime(start_date, fmt)
+                start_date = datetime.datetime.strptime(start_date, fmt).date()
             except Exception:
                 print("Try again.")
             else:
                 break
-        # End date
+
         while True:
             end_date = input("Enter end date (YYYYMMDD):  ")
             try:
-                end_date = datetime.datetime.strptime(end_date, fmt)
+                end_date = datetime.datetime.strptime(end_date, fmt).date()
             except Exception:
                 print("Try again.")
             else:
                 break
-        # Find and show
-        tasks_id = find_by_date_range(start_date, end_date)
-        result_menu(tasks_id)
+
+        ids = utils.find_by_date_range(start_date, end_date)
+        if ids:
+            result_menu(ids)
+        else:
+            print("No entries.")
+            time.sleep(1)
 
 
     def find_time_spent(self):
@@ -239,8 +235,12 @@ class SearchMenu(Menu):
             print("Try again.")
             return self.find_time_spent()
         else:
-            tasks_id = find_by_time_spent(minutes)
-            result_menu(tasks_id)
+            ids = utils.find_by_time_spent(minutes)
+            if ids:
+                result_menu(ids)
+            else:
+                print("No entries.")
+                time.sleep(1)
 
 
     def find_search_term(self):
@@ -249,25 +249,42 @@ class SearchMenu(Menu):
         # Query is searched for in title and notes
         # Go to ResultMenu
         query = input("Enter search term:  ")
-        tasks_id = find_by_search_term(query)
-        result_menu(tasks_id)
+        ids = utils.find_by_search_term(query)
+        if ids:
+            result_menu(ids)
+        else:
+            print("No entries.")
+            time.sleep(1)
 
 
 class ResultMenu(Menu):
 
-    def __init__(self, tasks_id):
-        pdb.set_trace()
+    def __init__(self, ids):
         super().__init__(
             OrderedDict([
                 ('n', self.next),
                 ('p', self.prev),
                 ('e', self.edit),
                 ('d', self.delete),
-                ('s', search_menu),
-                ('m', main_menu)]),
+                ('s', self.search_menu),
+                ('m', self.main_menu)]),
             "Result Menu")
-        self.tasks_id = tasks_id
+        self.ids = ids
         self.index = 0
+
+
+    @staticmethod
+    def search_menu():
+        """Go to search menu."""
+        search = SearchMenu()
+        search.menu_loop()
+
+
+    @staticmethod
+    def main_menu():
+        """Go to main menu."""
+        main = MainMenu()
+        main.menu_loop()
 
 
     def menu_loop(self):
@@ -282,18 +299,20 @@ class ResultMenu(Menu):
             choice = input("Which option do you choose?  ").lower().strip()
 
             if choice in self.menu:
-                #pdb.set_trace()
                 self.menu[choice]()
 
 
     def __len__(self):
-        return len(self.tasks_id)
+        return len(self.ids)
 
 
     def display(self):
         """Display a result including
         employee, task, minutes, notes, and date."""
-        task = get_task(self.tasks_id[self.index])
+        if not self.ids:
+            self.search_menu()
+        task = utils.get_task(self.ids[self.index])
+        print("\nEntry {} out of {}".format(self.index + 1, len(self)))
         print("\nEmployee:  " + task.employee)
         print("Taskname:  " + task.taskname)
         print("Minutes:   " + str(task.minutes))
@@ -317,15 +336,15 @@ class ResultMenu(Menu):
 
     def edit(self):
         """Edit current task."""
-        old_task_id = self.tasks_id[self.index]
+        old_id = self.ids[self.index]
         new_task = enter_task()
-        save_task(old_task_id, new_task)
+        utils.save_task(old_id, new_task)
 
 
     def delete(self):
         """Delete current task."""
-        delete_task(self.tasks_id[self.index])
-        del self.tasks_id[self.index]
+        utils.delete_task(self.ids[self.index])
+        del self.ids[self.index]
         # if deleting last one, loop around
         # otherwise, maintain index
         if self.index>=len(self):
